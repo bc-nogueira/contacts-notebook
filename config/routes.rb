6b04fd82b27e096b57abf6f02ee2cf1f904cde48
bc-nogueira <bcnogueira@id.uff.br>
2017-09-14 11:48:50 -0300

    Adding about page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+
+  # A member route will require an ID, because it acts on a member.
+  # A collection route doesn't because it acts on a collection of objects.
+  resources :pages, path: '', only: [] do
+    collection do
+      get :home, :about
+    end
+  end
 end
